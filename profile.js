4b47f64bbe60a5371913ea4e6601741c14184065
oyeniran samuel sanmi <sossesam@gmail.com>
2022-06-21 15:49:32 +0100

    done and running

---
+++ profile.js
@@ -0,0 +1,7 @@
+//Write a javascript code that prints out your name, height, and country on the screen
+
+var myName = "oyeniran samuel sanmi";
+var height = "5.8 ft";
+var country= "Nigeria"
+
+alert(myName + "\n" + height + "\n" + country)
